d3955e56dda277d2baa9dd87690e922a82a99c56
winstonwp <winstonwp@gmail.com>
2016-10-05 18:38:16 -0600

    BOOMSHAKALAKA with no code, must fix

--- app/plugins/kalabox-plugin-node/lib/node.js
+++ app/plugins/kalabox-plugin-node/lib/node.js
@@ -1,9 +1,9 @@
-
 'use strict';
 
 module.exports = function(kbox, app) {
 
   // Node modules
+  var fs = require('fs');
   var path = require('path');
   var id = kbox.core.deps.get('globalConfig').engineId;
   var group = kbox.core.deps.get('globalConfig').engineGid;
@@ -28,34 +28,12 @@ module.exports = function(kbox, app) {
   app.events.on('post-create', function(done) {
 
     //TODO ESTO PUEDE SER MUY UTIL PARA POBLAR DIRECTORIOS Y ETC
-    var baseExpressClone = getAppRunner();
-    var baseExpressUrl = 'https://github.com/winstonwp/helloworld-express.git';
-    baseExpressClone.opts.entrypoint = ['git'];
-    baseExpressClone.opts.cmd = [
-      'clone',
-      baseExpressUrl
-    ]
-    return kbox.engine.run(baseExpressClone)
-      .then(function(){
-        var installDeps = getAppRunner();
-        installDeps.opts.entrypoint = ['npm'];
-        installDeps.opts.cmd = [
-          '--prefix',
-          './helloworld-express/',
-          'install',
-          './helloworld-express/'
-        ]
-        kbox.engine.run(installDeps);
-      })
-      .nodeify();
-
   });
 
   /*
    * Add node specific CLI containers
    */
   app.events.on('post-app-load', function(app) {
-
     // Add node specific tasks
     var nodeCli = path.resolve(__dirname, '..', 'cli', 'node-cli.yml');
     app.taskFiles.push(nodeCli);
