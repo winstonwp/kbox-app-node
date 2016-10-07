bec24500af29a30059c35db916cc1d3d68c83a84
winstonwp <winstonwp@gmail.com>
2016-10-06 22:57:56 -0600

    BOOMSHAKALAKA and editing

---
+++ app/dockerfiles/node4/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+DIR="/var/www/html"
+if ! [ "$(ls -A $DIR)" ]; then
+  cd $DIR
+  git clone https://github.com/winstonwp/helloworld-express.git ./
+else
+  echo "$DIR had project..."
+fi
+if ! [ "$(ls -A $DIR/node_modules)" ]; then
+  cd $DIR
+  npm install
+else
+  echo "$DIR/node_modules is not empty"
+fi
+
+chown -Rf 1000:50 $DIR
+
+node "$DIR/index.js"
+
+exec "$@"
